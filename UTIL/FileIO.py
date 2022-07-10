96cf37c135071765473d7e83c99da0689f2d17ea
shah77pc <73152402+shah77pc@users.noreply.github.com>
2022-07-10 10:31:35 -0700

    Update FileIO.py

--- UTIL/FileIO.py
+++ UTIL/FileIO.py
@@ -6,7 +6,7 @@ from typing import NamedTuple
 
 
 def save_csv(df: pd.DataFrame, ticker: str, path: str):
-    file_path = os.path.join(path, ticker + '.csv').replace('\\', '/')
+    file_path = os.path.join(path, ticker + '.csv').replace('/', '/')
     df.to_csv(file_path, index=False)
 
 
