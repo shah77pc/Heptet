7665ad74ad947a5e225190eb0380f1556d72f182
shah77pc <73152402+shah77pc@users.noreply.github.com>
2022-07-10 10:48:53 -0700

    Update FetchData.py

--- EXAMPLE/FetchData.py
+++ EXAMPLE/FetchData.py
@@ -3,7 +3,7 @@ from UTIL import FileIO
 from DATA import API
 
 
-config_path = 'CONFIG\config_data.yml'
+config_path = 'CONFIG/config_data.yml'
 config      = FileIO.read_yaml(config_path)
 attr        = ['date', 'close']
 ticker_list = ['EURUSD', 'GBPUSD']
